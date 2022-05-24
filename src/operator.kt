67a9d9d9887f8b19b3306bc0fe7999771714fb6a
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-05-24 13:25:11 +0530

    operators

---
+++ src/operator.kt
@@ -0,0 +1,14 @@
+ fun main(){
+     val a=2
+     val b=4
+     println(a+b)
+     println(b-a) // this is the way to do simple operation
+     println(b*a)
+     println(b/a)
+      println(add(5,4))// both the function do same work
+      println(add1(5, 6))
+ }
+ fun add(a:Int,  b:Int):Int{
+      return a+b
+ }
+ fun add1(a:Int, b:Int)=a+b
