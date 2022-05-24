8411b3a3ee6ee12fcd10d60b6fd037934331bff1
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-05-24 17:37:48 +0530

    loops

---
+++ src/loop.kt
@@ -0,0 +1,8 @@
+fun main(){
+    for(i in 1..5){
+        print(i)
+    }//print the number in increasing number
+    println()
+    for(i in 5 downTo 1) print(i)
+    // print the number in reversing order
+}
